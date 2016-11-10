3dd8802f8adae7510294255c580c0ddc2e732492
ivan <ivan@orten.trion>
2016-11-10 16:16:17 +0200

    +

--- ya-5000.py
+++ ya-5000.py
@@ -8,16 +8,16 @@ z=1
 z2=1
 mp3=[]
 
-#~ #загружаем mp3, формируем |word - слово.mp3|
-#~ o_file=open('1.txt', 'a+')
-#~ read_file=o_file.readlines()
-#~ 
-#~ for i in read_file:
-	#~ destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.mp3'
-	#~ url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=mp3&lang=en-US&speaker=jane&key=2f6ede27-aaaf-42a1-a95b-34c67fc3fc9b&speed=0.1&emotion=good'
-	#~ urllib.urlretrieve(url, destination)
-#~ 
-#~ print "загрузка +"
+#загружаем mp3, формируем |word - слово.mp3|
+o_file=open('1.txt', 'a+')
+read_file=o_file.readlines()
+
+for i in read_file:
+	destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.mp3'
+	url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=mp3&lang=en-US&speaker=jane&key=2f6ede27-aaaf-42a1-a95b-34c67fc3fc9b&speed=0.1&emotion=good'
+	urllib.urlretrieve(url, destination)
+
+print "загрузка +"
 
 #создаем список файлов .MP3 рандомный
 for i in os.listdir("."):
