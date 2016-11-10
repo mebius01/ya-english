12cd8606c65e62b252bbabac23c1f8313362d550
ivan <ivan@orten.trion>
2016-11-10 16:13:34 +0200

    поправил цикл тишина +

--- ya-5000.py
+++ ya-5000.py
@@ -8,16 +8,16 @@ z=1
 z2=1
 mp3=[]
 
-##загружаем mp3, формируем |word - слово.mp3|
-#o_file=open('1.txt', 'a+')
-#read_file=o_file.readlines()
-
-#for i in read_file:
-	#destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.mp3'
-	#url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=mp3&lang=en-US&speaker=jane&key=2f6ede27-aaaf-42a1-a95b-34c67fc3fc9b&speed=0.1&emotion=good'
-	#urllib.urlretrieve(url, destination)
-
-#print "загрузка +"
+#~ #загружаем mp3, формируем |word - слово.mp3|
+#~ o_file=open('1.txt', 'a+')
+#~ read_file=o_file.readlines()
+#~ 
+#~ for i in read_file:
+	#~ destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.mp3'
+	#~ url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=mp3&lang=en-US&speaker=jane&key=2f6ede27-aaaf-42a1-a95b-34c67fc3fc9b&speed=0.1&emotion=good'
+	#~ urllib.urlretrieve(url, destination)
+#~ 
+#~ print "загрузка +"
 
 #создаем список файлов .MP3 рандомный
 for i in os.listdir("."):
@@ -25,16 +25,14 @@ for i in os.listdir("."):
 		mp3.append(i)
 random.shuffle(mp3)
 
-##добавляем тишину в mp3 файлы
-#mp3b=copy.deepcopy(mp3)
-#hush=open('hush', 'r'); a=hush.readlines()
-#for i in mp3b:
-	#filmp3=open(mp3b.pop(0), 'a')
-	#for ii in a:
-		#filmp3.writelines(ii)
-	#filmp3.close()
+#добавляем тишину в mp3 файлы
+hush=open('hush', 'r'); a=hush.readlines()
+for i in mp3:
+	filmp3=open(i, 'a')
+	filmp3.writelines(a)
+	filmp3.close()
 
-#print "тишина +"		
+print "тишина +"		
 
 while z < 250:
 	os.mkdir(str(z))
@@ -44,8 +42,8 @@ while z < 250:
 #заполняем файлами директории 1,2,3..200
 while z2 < 250:
 	if len(os.listdir(str(z2))) < 20:
-		shutil.copy(mp3.pop(0), str(z2))
+		shutil.move(mp3.pop(0), str(z2))
 	if len(os.listdir(str(z2))) == 20:
 		z2+=1
 
-print "файлы в норках"
+print "файлы в норках +"
