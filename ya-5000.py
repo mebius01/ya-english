b653a7d1279debb645fbc2281a82881240d40b9c
iv <iv@Ort.trion>
2017-02-08 17:17:28 +0200

    add def ListFile

--- ya-5000.py
+++ ya-5000.py
@@ -4,27 +4,27 @@
  
 import urllib, os, random, shutil, copy
 
-z=1
-z2=1
 mp3=[]
-
+#~ five='5000'
+#~ os.mkdir(five)
 #загружаем mp3, формируем |word - слово.mp3|
-o_file=open('1.txt', 'a+')
-read_file=o_file.readlines()
-
-for i in read_file:
-	destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.ogg'
-	url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=opus&lang=en-US&speaker=jane&key=f593fa88-b2a8-4c3e-8f12-834b060c722c&speed=0.1&emotion=good'
-	urllib.urlretrieve(url, destination)
+def Dow():
+	o_file=open('1.txt', 'a+')
+	read_file=o_file.readlines()
 	
+	for i in read_file:
+		destination = i.split(" - ")[0]+" - "+i.split(" - ")[1][:-1]+'.ogg'
+		url = 'https://tts.voicetech.yandex.net/generate?text='+i.split(" - ")[0]+'&format=opus&lang=en-US&speaker=jane&key=f593fa88-b2a8-4c3e-8f12-834b060c722c&speed=0.1&emotion=good'
+		urllib.urlretrieve(url, destination)
+	print "загрузка +"
 
-print "загрузка +"
 
 #создаем список файлов .ogg рандомный
-for i in os.listdir("."):
-	if "ogg" in i:
-		mp3.append(i)
-#random.shuffle(mp3)
+def Ogg():
+	for i in os.listdir("."):
+		if "ogg" in i:
+			mp3.append(i)
+	#random.shuffle(mp3)
 
 #добавляем тишину в mp3 файлы
 #hush=open('hush', 'r'); a=hush.readlines()
@@ -34,16 +34,41 @@ for i in os.listdir("."):
 #	filmp3.close()
 #print "тишина +"		
 
-while z < 500:
-	os.mkdir(str(z))
-	z+=1
+# создание директорий и заполняем файлами 1,2,3..200
+def MkDir():
+	z=1
+	z2=1
+	while z < 500:
+		os.mkdir(str(z))
+		z+=1
 
+	while z2 < 500:
+		if len(os.listdir(str(z2))) < 10:
+			shutil.move(mp3.pop(0), str(z2))
+		if len(os.listdir(str(z2))) == 10:
+			z2+=1
+	
+	print "файлы в норках +"
 
-#заполняем файлами директории 1,2,3..200
-while z2 < 500:
-	if len(os.listdir(str(z2))) < 10:
-		shutil.move(mp3.pop(0), str(z2))
-	if len(os.listdir(str(z2))) == 10:
-		z2+=1
+# создание файлов вида ang - russ, ang, russ
+def ListFile():
+	for i in os.listdir("./5000"):
+		for ii in os.listdir("./5000/"+i):
+			print ii[:-4]
+			ang_rus_file=open('./5000/'+i+'/ang-rus.txt', 'a')
+			ang_rus_file.write(ii[:-4]+'\n')
+			ang_rus_file.close()
+			
+			a = ii.split(' - ')
+			
+			ang_rus_file=open('./5000/'+i+'/ang.txt', 'a')
+			ang_rus_file.write(a[0]+'\n')
+			ang_rus_file.close()
+			
+			ang_rus_file=open('./5000/'+i+'/rus.txt', 'a')
+			ang_rus_file.write(a[-1][:-4]+'\n')
+			ang_rus_file.close()
+			print a[0]
+			print a[-1][:-4]
 
-print "файлы в норках +"
+ListFile()
