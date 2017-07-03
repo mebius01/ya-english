a17e4442b59d2f4dda81bbdc591c1bb846185762
ivan <ivan@W>
2017-07-03 23:33:53 +0300

    sorry Donald Ervin Knuth

--- read-trlate.py
+++ read-trlate.py
@@ -1,26 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""
+Прости меня Кнут
+Прости меня Вирт
+Ибо я грешен 
+Грех мой велик
+"""
+
 import os, re
+from itertools import groupby
 
 tex=open('text.txt', 'r')
-tex_split=tex.readline(); tex_split=tex_split.split(" ")
+tex_split=tex.readline()
+tex_split=tex_split.split(" ")
+new_x = list(set(tex_split))
 tex.close()
+
 
 def WhileWord():
 	x=[]
+	y=[]
 	zero=1
 	while zero < 500:
 		a=os.listdir("./5000/"+str(zero))
 		for i in a:
-			x.append(i)
+			result = re.search('^\w*\S', i)
+			x.append(result.group(0))
+			y.append(os.path.abspath(i))
 		zero=zero+1
-	return x
+	return x, y
 
+print type(WhileWord())
 
-for i in tex_split:
-	for ii in WhileWord():
+tex_list=[]
+for i in new_x:
+	for ii in WhileWord()[0]:
 		if i == ii:
-			print i, ii
+			tex_list.append((i, ii, WhileWord()[0].index(ii), WhileWord()[1][WhileWord()[0].index(ii)]))
+			#~ print i, ii, WhileWord()[0].index(ii), WhileWord()[1][WhileWord()[0].index(ii)]
+
+for i in list(set(tex_list)):
+	for ii in i:
+		print ii
+
 
 
 
